8d2e1b3d3f6f929ab46478b742acb86af5c8386c
Kieran Barker <kieran.barker@multiverse.io>
2025-07-22 18:25:13 +0100

    Fix indentation

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,28 +2,28 @@ require("dotenv").config({ quiet: true });
 const jwt = require("jsonwebtoken");
 
 function auth(req, res, next) {
-	const header = req.get("Authorization");
+  const header = req.get("Authorization");
 
-	if (!header) {
-		res.set("WWW-Authenticate", "Bearer").sendStatus(401);
-		return;
-	}
+  if (!header) {
+    res.set("WWW-Authenticate", "Bearer").sendStatus(401);
+    return;
+  }
 
-	const [scheme, token] = header.split(" ");
+  const [scheme, token] = header.split(" ");
 
-	if (scheme !== "Bearer" || !token) {
-		res.sendStatus(401);
-		return;
-	}
+  if (scheme !== "Bearer" || !token) {
+    res.sendStatus(401);
+    return;
+  }
 
-	try {
-		const decoded = jwt.verify(token, process.env.JWT_KEY);
-		req.user_id = decoded.user_id;
-		next();
-	} catch (error) {
-		console.error(error);
-		res.sendStatus(401);
-	}
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    req.user_id = decoded.user_id;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(401);
+  }
 }
 
 module.exports = auth;
